aa5d4d540d13bf0a7bf14c5f3fd46548a1bf5d92
Joel Watkins <jw@Joels-MacBook-Pro.local>
2020-12-30 22:55:10 -0700

    finished js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,45 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+// Create References
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+// Input Data to html
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column]))
+    });
+}
+
+addData(tableData);
+
+// Create an Event Listener for the Button
+button.on("click", () => {
+
+    d3.event.preventDefault();
+
+    var inputDate = inputFieldDate.property("value").trim();
+
+    var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
+
+    $tbody.html("");
+
+    let response = {
+        filterDate
+    }
+
+    if (response.filterDate.length !== 0) {
+        addData(filterDate);
+    }
+
+    // Top if only works for filtering the date
+    else {
+        $tbody.append("tr").append("td").text("No Luck, Try Again.");
+    }
+})
